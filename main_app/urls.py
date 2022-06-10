13bddd2c36de620b80f024810630707f5d81727e
Zarema Tassyrova <zarema.tsr@gmail.com>
2022-06-10 14:58:58 -0400

    full crud

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
   path('', views.Home.as_view(), name="home"),
   path('about/', views.About.as_view(), name="about"),
   path('cats/', views.CatList.as_view(), name="cat_list"),
+  path('cats/new', views.CatCreate.as_view(), name="cat_create"),
+  path('cats/<int:pk>/', views.CatDetail.as_view(), name="cat_detail"),
+  path('cats/<int:pk>/update',views.CatUpdate.as_view(), name="cat_update"),
+  path('cats/<int:pk>/delete',views.CatDelete.as_view(), name="cat_delete"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,31 +1,51 @@
-# from django.shortcuts import render
-# from django.views import View
-# from django.http import HttpResponse
+
 from django.views.generic.base import TemplateView
 from .models import Cat
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
 
 # Create your views here.
 
 
 class Home(TemplateView):
-    template_name="home.html"
-    
+    template_name = "home.html"
 
 
 class About(TemplateView):
-  template_name = "about.html"
-  
-  class Cat:
-      def __init__(self, breed, image, description):
-        self.breed = breed
-        self.image = image
-        self.description = description
-        
-        
-        
+    template_name = "about.html"
+
+    class Cat:
+        def __init__(self, breed, image, description):
+            self.breed = breed
+            self.image = image
+            self.description = description
+
+
 class CatList(TemplateView):
     template_name = "cat_list.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["cats"] = Cat.objects.all() 
+        context["cats"] = Cat.objects.all()
         return context
+
+class CatCreate(CreateView):
+        model = Cat
+        fields = ['breed', 'img', 'description', 'verified_cat']
+        template_name = "cat_create.html"
+        success_url = "/cats/"
+        
+class CatDetail(DetailView):
+        model = Cat
+        template_name = "cat_detail.html"   
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'img', 'description', 'verified_cat']
+    template_name = "cat_create.html"
+    success_url = "/cats/"
+    
+class CatDelete(DeleteView):
+    model = Cat
+    template_name = "cat_delete.html"
+    success_url = "/cats/"
